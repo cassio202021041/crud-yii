489e7450bba292d49eeeb4378beba0d1e0473fbf
cassio202021041 <cassio00710@hotmail.com>
2025-01-16 23:06:04 -0300

    CRUD-YII2-01 - ADD FormController, formCadastro na model

--- crud-yii/controllers/FormController.php
+++ crud-yii/controllers/FormController.php
@@ -9,6 +9,11 @@ class FormController extends \yii\web\Controller
     public function actionCadastro()
     {
         $model = new CadastroForm;
+
+        if($model->load(yii::$app->request->post()) && $model->validate()){
+
+        }
+
         return $this->render('cadastro', [
             'model' => $model
         ]);
--- crud-yii/views/form/Cadastro.php
+++ crud-yii/views/form/Cadastro.php
@@ -1,2 +1,23 @@
 <?php 
-var_dump($model);
+use yii\bootstrap5\ActiveForm;
+use yii\helpers\Html;
+
+$this->title = 'Cadastro';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<h1>Cadastro de Usuário</h1>
+<p>Cadastre-se no site</p>
+<div class="row">
+    <div class="col-lg-5">
+    <?php $form = ActiveForm::begin(); ?>
+<?= $form->field($model, 'nome'); ?>
+<?= $form->field($model, 'email'); ?>
+<?= $form->field($model, 'telefone'); ?>
+<?= Html::submitButton('Cadastrar', ['class' => 'btn btn-primary']); ?>
+<?php ActiveForm::end(); ?>
+
+
+
+    </div>
+
+</div>
